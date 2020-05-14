fbdec4131c67bd73b2a6a3da28e8125eecd103c3
Elías Angulo <crossplatformdev@users.noreply.github.com>
2020-05-15 01:42:38 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -72,7 +72,7 @@ echo "Restart apache service"
 echo ""
 sudo service apache2 restart
 sudo rm -f /var/www/html/GumCP/installer.sh
-sudo chmod 777 /var/www/html/GumCP/include/config.php
+sudo chmod 777 /var/www/GumCP/include/config.php
 echo ""
 echo "Done"
 echo ""
